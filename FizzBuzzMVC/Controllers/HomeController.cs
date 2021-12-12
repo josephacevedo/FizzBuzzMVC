1ed6793792fec959fe5c20e96f2e024d0abab4d0
Joseph Acevedo <josephacevedo1997@gmail.com>
2021-12-11 22:58:32 -0600

    finished fizzbuzz

--- FizzBuzzMVC/Controllers/HomeController.cs
+++ FizzBuzzMVC/Controllers/HomeController.cs
@@ -23,6 +23,52 @@ namespace FizzBuzzMVC.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult FBPage()
+        {
+            FizzBuzz model = new();
+
+            model.FizzValue = 3;
+            model.BuzzValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FBPage(FizzBuzz fizzbuzz)
+        {
+            List<string> fbItems = new();
+
+            bool fizz;
+            bool buzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                fizz = (i % fizzbuzz.FizzValue == 0);
+                buzz = (i % fizzbuzz.BuzzValue == 0);
+
+                if (fizz == true && buzz == true)
+                {
+                    fbItems.Add("FizzBuzz");
+                }
+                else if (fizz == true)
+                {
+                    fbItems.Add("Fizz");
+                }
+                else if (buzz == true)
+                {
+                    fbItems.Add("Buzz");
+                }
+                else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+            fizzbuzz.Result = fbItems;
+            return View(fizzbuzz);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
